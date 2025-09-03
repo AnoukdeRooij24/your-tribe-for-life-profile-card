8b6e80fb3f1c463d05521494c3d26e188cff598d
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-09-03 15:27:51 +0200

    Project opgezet met SvelteKit en data opgehaald gefilterd op tweedejaars #1

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { data } = $props(); 
+    const members = data.members;
+</script>
+
+<h1>Test</h1>
+<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+
+{#each members as member}
+    <a href="/{member.id}"> {member.name} </a>
+{/each}
+
