57231d4115f5042a6960806f4e5e0d1ac6908e39
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-09-11 11:03:13 +0200

    I love web toegevoegd #2 #4

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -13,6 +13,7 @@
 
     <div class="info">
         <p> <strong>Github:</strong> { member.github_handle } </p>
+        <p> <strong>I Love Web:</strong> <a href=" { member.website } "> I Love Web </a> </p>
         <p> <strong>Bio:</strong> { member.bio } </p>
         <p> <strong>Birthdate:</strong> { member.birthdate } </p>
         <p> <strong>Fav emoji:</strong> { member.fav_emoji } </p>
