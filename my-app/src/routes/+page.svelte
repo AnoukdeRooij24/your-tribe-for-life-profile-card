196a899fbd6d5777ea1b713f4d77337370fbcf0f
AnoukdeRooij24 <anouk.de.rooij@hva.nl>
2025-09-11 10:54:28 +0200

    mediaquery responsive + tekst gecentreerd #4

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -4,15 +4,21 @@
 </script>
 
 <article>
-    <h1> { member.name }</h1>
-    <img src={member.avatar} alt="{member.name} picture" width="100px" height="100px"/>
+    <div class="name">
+        <img src={member.avatar} alt="{member.name} picture" width="100px" height="100px"/>
+        <h1> { member.name }</h1>
+    </div>
 
-    <p> <strong>Github:</strong> { member.github_handle } </p>
-    <p> <strong>Bio:</strong> { member.bio } </p>
-    <p> <strong>Birthdate:</strong> { member.birthdate } </p>
-    <p> <strong>Fav emoji:</strong> { member.fav_emoji } </p>
-    <p> <strong>Hobby:</strong> { member.fav_hobby } </p>
-    <p> <strong>Fav animal:</strong> { member.fav_animal } </p>
+    <div class="line"></div>
+
+    <div class="info">
+        <p> <strong>Github:</strong> { member.github_handle } </p>
+        <p> <strong>Bio:</strong> { member.bio } </p>
+        <p> <strong>Birthdate:</strong> { member.birthdate } </p>
+        <p> <strong>Fav emoji:</strong> { member.fav_emoji } </p>
+        <p> <strong>Hobby:</strong> { member.fav_hobby } </p>
+        <p> <strong>Fav animal:</strong> { member.fav_animal } </p>
+    </div>
 </article>
 
 
